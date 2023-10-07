5c2baf5e2fe994e6f81623a327090bdf578cb229
DKD <126566259+D-K-Deng@users.noreply.github.com>
2023-10-06 20:22:40 -0400

    Update protein_optimize.py

--- protein_optimize.py
+++ protein_optimize.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1Tt8C4luBrg329PavxfKvxC6b-BgmE3LR
 """
 
-#@title Main Process_Manual
+#@title Main Process
 import locale
 def getpreferredencoding(do_setlocale = True):
     return "UTF-8"
@@ -552,17 +552,3 @@ if os.path.isfile(excel_file_path):
     print(f"The Excel file '{excel_file_name}' is located at: {excel_file_path}")
 else:
     print(f"The Excel file '{excel_file_name}' was not found in the current directory.")
-
-#@title Package and download results
-#@markdown If you are having issues downloading the result archive, try disabling your adblocker and run this cell again. If that fails click on the little folder icon to the left, navigate to file: `jobname.result.zip`, right-click and select \"Download\" ).
-
-if msa_mode == "custom":
-  print("Don't forget to cite your custom MSA generation method.")
-
-files.download(f"{jobname}.result.zip")
-
-if save_to_google_drive == True and drive:
-  uploaded = drive.CreateFile({'title': f"{jobname}.result.zip"})
-  uploaded.SetContentFile(f"{jobname}.result.zip")
-  uploaded.Upload()
-  print(f"Uploaded {jobname}.result.zip to Google Drive with ID {uploaded.get('id')}")
